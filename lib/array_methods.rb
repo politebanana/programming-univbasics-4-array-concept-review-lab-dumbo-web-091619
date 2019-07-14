3c474ce82f6e507a6eb43eb232a58d61759a7a53
Christian Zambrana <chrislajas@gmail.com>
2019-07-14 23:40:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   array do |count|
-    if array[count] = value_to_find
+    if array[count] == value_to_find
       return count
 end
 
